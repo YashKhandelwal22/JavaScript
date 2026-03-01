8e60a4adc4b468c73d7fd382f9f85ec32ac6d3e3
YashKhandelwal22 <166475386+YashKhandelwal22@users.noreply.github.com>
2026-03-01 12:02:35 +0000

    datatypes

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // use all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// console.log(3
+//     +
+//     3) // code readability should be high
+
+let name = "Yash" //String
+let age = 23 // integer
+let isLoggedIn = false // boolean
+
+// Number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+// console.log(typeof "Yash"); //string
+// console.log(typeof age); //number
+// console.log(typeof null); //object
+console.log(typeof undefined); // undefined
