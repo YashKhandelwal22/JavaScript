091451b98455ecd331ba941ea62a77c0ffbe24fe
YashKhandelwal22 <166475386+YashKhandelwal22@users.noreply.github.com>
2026-04-13 12:18:05 +0000

    conversion of datatypes

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -30,3 +30,43 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+
+// *********************************Operations**********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " yash"
+let str3 = str1+str2
+console.log(str3);
+
+
+console.log("1"+2); // in this the output will be 12 because 1 is in string and it comes first
+console.log(1+"2"); // in this the output will be 12 because before 2 there is only 1 and 2 is in string
+console.log("1"+2+2);  // in this the output will be 122 because 1 is in string and it comes first
+console.log(1+2+"2"); // in this the output will be 32 because 1 and 2 is number it will get added and the other 2 is string so it will not get added
+
+console.log(true);
+console.log(+true); // it will give 1 only
+// console.log(true+); // it will give error
+console.log(+""); // it will give 0 only
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
